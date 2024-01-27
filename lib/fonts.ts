828eec130c130fd9d880e08d2cceaa8170f90f4a
harshithpabbati <pabbatiharshith@gmail.com>
2024-01-27 20:33:42 +0530

    chore: use mabrypro font

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,14 +1,7 @@
-import {
-  JetBrains_Mono as FontMono,
-  Inter as FontSans,
-} from 'next/font/google';
+import localFont from 'next/font/local';
 
-export const fontSans = FontSans({
-  subsets: ['latin'],
+export const fontSans = localFont({
+  src: './MabryPro-Regular.woff2',
   variable: '--font-sans',
-});
-
-export const fontMono = FontMono({
-  subsets: ['latin'],
-  variable: '--font-mono',
+  display: 'swap',
 });
